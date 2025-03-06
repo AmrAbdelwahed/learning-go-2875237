7953fa6c30e20622a31cb0372ac03c664c788d00
AmrAbdelwahed <amrabdelwahed00@gmail.com>
2025-03-06 15:05:47 -0500

    initial commit

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+	fmt.Println(colors)
+	fmt.Println(colors[1])
+
+	var number = [6]int{5, 2, 3, 4, 5, 6}
+	// or
+	var numberDynamic = []int{5, 2, 3, 4}
+
+	fmt.Println(number)
+
+	fmt.Println("Number of colors:", len(colors))
+	fmt.Println("Number of integers:", len(number))
+	fmt.Println("Number of integers (dynamic):", len(numberDynamic))
 }
